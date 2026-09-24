86832f46bc32610b864857fb87954cfc4b70e991
Gopiji1122 <167205565+Gopiji1122@users.noreply.github.com>
2026-09-24 11:25:42 +0530

    Delete assets/js/calculator-schema-test-data.js

--- assets/js/calculator-schema-test-data.js
+++
@@ -1,9 +0,0 @@
-window.EMIFORMULA_CALCULATOR_SCHEMA = {
-  "@context": "https://schema.org",
-  "@type": "WebApplication",
-  "name": "EMI Calculator",
-  "description": "Calculate loan EMI, total interest and total repayment.",
-  "url": "https://gopiji1122.github.io/Emiformula/pages/calculator-schema-test.html",
-  "applicationCategory": "FinanceApplication",
-  "operatingSystem": "Web"
-};
