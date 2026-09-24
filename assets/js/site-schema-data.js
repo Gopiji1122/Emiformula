3a9a55f165d3325ba4b0247c3c4e4384cb6c0189
Gopiji1122 <167205565+Gopiji1122@users.noreply.github.com>
2026-09-24 14:17:46 +0530

    Create site-schema-data.js

---
+++ assets/js/site-schema-data.js
@@ -0,0 +1,18 @@
+window.EMIFORMULA_SITE_SCHEMA = [
+
+  {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    "name": "EMIFORMULA",
+    "description": "EMI and loan calculation tools for simple and practical financial calculations.",
+    "url": "https://gopiji1122.github.io/Emiformula/"
+  },
+
+  {
+    "@context": "https://schema.org",
+    "@type": "Organization",
+    "name": "EMIFORMULA",
+    "url": "https://gopiji1122.github.io/Emiformula/"
+  }
+
+];
