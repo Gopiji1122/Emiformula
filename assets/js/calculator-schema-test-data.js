ebbe5a913ced996c54378eb25e420c3b1a4a966a
Gopiji1122 <167205565+Gopiji1122@users.noreply.github.com>
2026-09-25 10:24:41 +0530

    Update calculator-schema-test-data.js

--- assets/js/calculator-schema-test-data.js
+++ assets/js/calculator-schema-test-data.js
@@ -1,9 +1,18 @@
 window.EMIFORMULA_CALCULATOR_SCHEMA = {
   "@context": "https://schema.org",
   "@type": "WebApplication",
+
   "name": "EMI Calculator",
-  "description": "Calculate loan EMI, total interest and total repayment.",
-  "url": "https://gopiji1122.github.io/Emiformula/pages/calculator-schema-test.html",
-  "applicationCategory": "FinanceApplication",
-  "operatingSystem": "Web"
+
+  "description":
+    "Calculate monthly EMI, total interest, total payment and loan repayment details.",
+
+  "url":
+    "https://gopiji1122.github.io/Emiformula/calculators/emi-calculator.html",
+
+  "applicationCategory":
+    "FinanceApplication",
+
+  "operatingSystem":
+    "Web"
 };
